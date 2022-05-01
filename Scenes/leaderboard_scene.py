c4d2f3238dcb2f037507373c5df647aa21ff1a69
Robin Walter <mfg.robinwalter@googlemail.com>
2022-05-01 13:31:44 +0200

    add logic for redrawing ui in leaderboard

--- Scenes/leaderboard_scene.py
+++ Scenes/leaderboard_scene.py
@@ -18,6 +18,7 @@ class LeaderboardScene(Scene):
         self.leader_manager = pygame_gui.UIManager((1200, 800), 'theme.json')
         self.back_button = gui_elements.createButton((0,350),'BACK',globals.buttonTypes['ACCEPT'], self.leader_manager)
         self.currentleaderboardarray = []
+        self.currentUiElements = []
         #self.rules_label = gui_elements.createTextfeld((200,150),"text",globals.textboxTypes['RULES'], self.leader_manager)
 
         self.easy_button = gui_elements.createButton((300,50),'EASY',globals.buttonTypes['STRENGTH'], self.leader_manager)
@@ -51,20 +52,26 @@ class LeaderboardScene(Scene):
         return ausgabe
     
     def drawleaderboard(self):
+
         offset_y = 250
         offset_x = 350
 
-        gui_elements.createTextfeld((offset_x,offset_y-50),'USERNAME',globals.textboxTypes['DATA'], self.leader_manager)
-        gui_elements.createTextfeld((offset_x+100,offset_y-50),'WINS',globals.textboxTypes['DATA'], self.leader_manager)
-        gui_elements.createTextfeld((offset_x+100*2,offset_y-50),'LOSSES',globals.textboxTypes['DATA'], self.leader_manager)
+        for uiElement in self.currentUiElements:
+            uiElement.kill()
+
+        self.currentUiElements = []
+
+        self.currentUiElements.append(gui_elements.createTextfeld((offset_x,offset_y-50),'USERNAME',globals.textboxTypes['DATA'], self.leader_manager))
+        self.currentUiElements.append(gui_elements.createTextfeld((offset_x+100,offset_y-50),'WINS',globals.textboxTypes['DATA'], self.leader_manager))
+        self.currentUiElements.append(gui_elements.createTextfeld((offset_x+100*2,offset_y-50),'LOSSES',globals.textboxTypes['DATA'], self.leader_manager))
 
 
         for index,element in enumerate(self.currentleaderboardarray):
             result = element[0]+":"+str(element[1])+" / "+str(element[2])
             #print(result)
-            gui_elements.createTextfeld((offset_x,offset_y+50*index),element[0],globals.textboxTypes['DATA'], self.leader_manager)
-            gui_elements.createTextfeld((offset_x+100,offset_y+50*index),str(element[1]),globals.textboxTypes['DATA'], self.leader_manager)
-            gui_elements.createTextfeld((offset_x+100*2,offset_y+50*index),str(element[2]),globals.textboxTypes['DATA'], self.leader_manager)
+            self.currentUiElements.append(gui_elements.createTextfeld((offset_x,offset_y+50*index),element[0],globals.textboxTypes['DATA'], self.leader_manager))
+            self.currentUiElements.append(gui_elements.createTextfeld((offset_x+100,offset_y+50*index),str(element[1]),globals.textboxTypes['DATA'], self.leader_manager))
+            self.currentUiElements.append(gui_elements.createTextfeld((offset_x+100*2,offset_y+50*index),str(element[2]),globals.textboxTypes['DATA'], self.leader_manager))
 
 
 
@@ -79,13 +86,13 @@ class LeaderboardScene(Scene):
                                 self.manager.goTo(Scenes.mainmenue_scene.MainMenueScene())
                             elif event.ui_element == self.easy_button:
                                 print('leaderboard for easy')
-                                self.getleaderboard('easy')
+                                self.getleaderboard('EASY')
                             elif event.ui_element == self.medium_button:
                                 print('leaderboard for medium')
-                                self.getleaderboard('medium')
+                                self.getleaderboard('MEDIUM')
                             elif event.ui_element == self.hard_button:
                                 print('leaderboard for hard')
-                                self.getleaderboard('hard')
+                                self.getleaderboard('HARD')
                     #if event.user_type == pygame_gui.UI_TEXT_ENTRY_FINISHED: 
                         #if event.ui_element == self.username_input:
                             #username = self.username_input.get_text()
